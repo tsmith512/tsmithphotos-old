4fc720c7742dd7afea117154a75062e94089a441
Taylor Smith <tsmith@tsmithcreative.com>
2016-07-05 21:02:13 +0000

    Expand the original photo task to add medium and thumbnail images

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp');
 
 var imagemin = require('gulp-imagemin');
+var resize = require('gulp-image-resize');
 var rename = require("gulp-rename");
 
-gulp.task('images-fullsize', function() {
+gulp.task('photos', function() {
   return gulp.src('source/Photography/**/*.jpg')
     .pipe(rename(function (path) {
       // Sometimes I use subdirectories within albums to denote days, squash em
@@ -11,8 +12,13 @@ gulp.task('images-fullsize', function() {
       // cameras both don't cycle until 9999 so only if 10,000 were taken in a day.
       path.dirname = path.dirname.split('/')[0];
     }))
-    .pipe(imagemin())
-    .pipe(gulp.dest('_site/photo/original/'));
+    .pipe(imagemin({progressive: true}))
+    .pipe(gulp.dest('_site/photo/original/'))
+    .pipe(resize({width: 600, height: 600, crop: false, upscale: false}))
+    .pipe(gulp.dest('_site/photo/medium/'))
+    .pipe(resize({width: 200, height: 200, crop: true, upscale: false}))
+    .pipe(gulp.dest('_site/photo/thumb/'))
+    // @TODO: Can we do that thing Rupl used to do with blurry 10px images for a pre-load?
 });
 
 gulp.task('default', function() {});
