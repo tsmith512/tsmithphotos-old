342e69caab7037783a2643f46d97b69054361735
Taylor Smith <tsmith@tsmithcreative.com>
2016-07-11 20:06:41 +0000

    Fire up PhotoSwipe when a thumbnail is clicked, at that particular index

--- _js/photoswipe.tsp.js
+++ _js/photoswipe.tsp.js
@@ -6,41 +6,63 @@
 (function(){
   'use strict';
 
+  // A container for all gallery items on this page
+  var items = [];
 
-  document.addEventListener("DOMContentLoaded", function() {
-    var pswpElement = document.querySelectorAll('.pswp')[0];
-    var items = [];
+  function galleryItems() {
     var photos = document.querySelectorAll('.album a');
     var total = photos.length;
 
     for (var i = 0; i < total; i++) {
-        var photo = photos[i]; // This element is the link tag, not the thumbnail image.
-        
-        // Include only elements
-        if (photo.nodeType !== 1) { continue; }
+      var photo = photos[i]; // This element is the link tag, not the thumbnail image.
+      
+      // Include only elements
+      if (photo.nodeType !== 1) { continue; }
 
-        var size = photo.getAttribute('data-size').split('x');
+      var size = photo.getAttribute('data-size').split('x');
 
-        var item = {
-            src: photo.getAttribute('href'),
-            w: parseInt(size[0], 10),
-            h: parseInt(size[1], 10),
-            msrc: photo.getAttribute('href').replace('original', 'medium'),
-        };
+      var item = {
+        src: photo.getAttribute('href'),
+        w: parseInt(size[0], 10),
+        h: parseInt(size[1], 10),
+        msrc: photo.getAttribute('href').replace('original', 'medium'),
+      };
 
-        items.push(item);
+      items.push(item);
     }
-    console.log(items);
+  }
+
+  document.addEventListener("DOMContentLoaded", galleryItems);
+
+  function launchLightbox(e) {
+    e.preventDefault();
+
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // Get the <li> of the link
+    var parent = this.parentNode;
+
+    // Get the <ul> of the link
+    var list = parent.parentNode;
+
+    // Need to get the index of this LI within the UL
+    var index = Array.prototype.indexOf.call(list.children, parent);
+
     // define options (if needed)
     var options = {
-        // optionName: 'option value'
-        // for example:
-        // index: 0 // start at first slide
+      index: index // start at first slide
     };
 
     // Initializes and opens PhotoSwipe
     var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
     gallery.init();
-  });
+  };
+
+  // Loop through photos in the album and attach the above initializer
+  var photos = document.querySelectorAll(".album a");
+  for (var i = 0; i < photos.length; i++) {
+    var current = photos[i];
+    current.addEventListener('click', launchLightbox, false);
+  }
   
 })();
